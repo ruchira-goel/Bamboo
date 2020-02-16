922cea9ead1999acde430b88e50e8ef1ef9381cc
Hajera Zemy <hzemy@purdue.edu>
2020-02-16 18:56:00 -0500

    Created basic layout for Meal and NutritionInfo classes

--- Backend/Bamboo/src/main/java/com/bamboo/demo/Models/Meal.java
+++ Backend/Bamboo/src/main/java/com/bamboo/demo/Models/Meal.java
@@ -1,4 +1,37 @@
 package com.bamboo.demo.Models;
 
+import java.util.Date;
+
 public class Meal {
+    private String id;
+    private String name;
+    private NutritionInfo nutritionInfo;
+
+    public void addToDate(Date date) {
+        //TODO
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public NutritionInfo getNutritionInfo() {
+        return nutritionInfo;
+    }
+
+    public void setNutritionInfo(NutritionInfo nutritionInfo) {
+        this.nutritionInfo = nutritionInfo;
+    }
 }
