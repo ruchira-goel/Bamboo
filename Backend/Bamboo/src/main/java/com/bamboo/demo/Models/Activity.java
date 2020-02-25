b8dda6db8e7f1540438a2293e2f899933ee64516
Ramya Mandyam Anandampullai <42981906+rmandyam1@users.noreply.github.com>
2020-02-25 17:48:19 -0500

    Add Activity class

---
+++ Backend/Bamboo/src/main/java/com/bamboo/demo/Models/Activity.java
@@ -0,0 +1,24 @@
+package com.bamboo.demo.Models;
+
+public class Activity {
+    private String id;
+    private String type;
+    private int calories;
+    private int minutes;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+}
