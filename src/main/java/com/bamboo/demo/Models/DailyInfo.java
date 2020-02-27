f8ff918e658ffbdc4c6714b264a17b804de242d5
Ramya Mandyam Anandampullai <42981906+rmandyam1@users.noreply.github.com>
2020-02-27 10:05:38 -0500

    Adding Exercise input-related classes

--- src/main/java/com/bamboo/demo/Models/DailyInfo.java
+++ src/main/java/com/bamboo/demo/Models/DailyInfo.java
@@ -1,4 +1,31 @@
 package com.bamboo.demo.Models;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class DailyInfo {
+    private String id;
+    private Date date;
+    private ArrayList<String> meals;
+    private ArrayList<String> activities;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public ArrayList<String> getMeals() {
+        return meals;
+    }
+
+    public ArrayList<String> getActivities() {
+        return activities;
+    }
+
+    public void addMeals(String meal) {
+        meals.add(meal);
+    }
+
+    public void addActivities(String activity) {
+        activities.add(activity);
+    }
 }
