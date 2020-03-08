285858722cec8075119ced8ddfdc1fed4a72a99a
Ramya Mandyam Anandampullai <42981906+rmandyam1@users.noreply.github.com>
2020-03-08 18:19:50 -0400

    Backend for Calorie Calculations

--- Backend/Bamboo/src/main/java/com/bamboo/demo/Handlers/ActivityHandler.java
+++ Backend/Bamboo/src/main/java/com/bamboo/demo/Handlers/ActivityHandler.java
@@ -22,24 +22,58 @@ public class ActivityHandler {
         this.userRepo = userRepo;
         this.dailyInfoRepo = dailyInfoRepo;
         this.activityRepo = activityRepo;
-
     }
 
-
-    public Activity saveActivity(String email, String activityName, int time, int calories) throws IOException, JSONException {
+    // Distance is in miles, time is in minutes
+    public Activity saveActivity(String email, String activityName, int time, int calories, double distance) throws IOException, JSONException {
         User user = this.userRepo.findByEmail(email).get();
         String userId = user.getUserId();
 
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
         Date currentDate = new Date(System.currentTimeMillis());
         String date = formatter.format(currentDate);
+
         Activity activity = new Activity(userId, activityName, calories, time);
+        double MET = findMET(activity, distance);
+        int newCalories = (int) (MET * user.getWeight());
+        activity.setCalories(newCalories);
         this.activityRepo.save(activity);
 
         System.out.println("Date: " + date);
         addToDate(date, activity);
 
         return activity;
+    }
+
+    public double findMET(Activity activity, double distance) {
+        //HashMap<String, Double> MET = activity.getMETValues();
+        if (activity.getType().equalsIgnoreCase("running")) {
+            double[] runningSpeeds = {4.0, 5.0, 5.2, 6.0, 6.7, 7.0, 7.5, 8, 8.6, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0};
+            // distance matters
+            // speed = mph
+            double speed = distance / (activity.getMinutes() / 60.0);
+            if (speed > runningSpeeds[runningSpeeds.length - 1]) {
+                speed = runningSpeeds[runningSpeeds.length - 1];
+            } else {
+                for (double runningSpeed : runningSpeeds) {
+                    if (speed <= runningSpeed) {
+                        speed = runningSpeed;
+                    }
+                }
+            }
+            double MET = 0.0;
+            if (activity.getMETValues().containsKey(activity.getType().toLowerCase() + " " + String.format("%.2f",speed))) {
+                MET = activity.getMETValues().get(activity.getType().toLowerCase() + " " + String.format("%.2f",speed));
+            }
+            return MET;
+        } else {
+            //distance v=can be ignored
+            double MET = 0.0;
+            if (activity.getMETValues().containsKey(activity.getType().toLowerCase())) {
+                MET = activity.getMETValues().get(activity.getType().toLowerCase());
+            }
+            return MET;
+        }
     }
 
 
--- Backend/Bamboo/src/main/java/com/bamboo/demo/Models/Activity.java
+++ Backend/Bamboo/src/main/java/com/bamboo/demo/Models/Activity.java
@@ -4,6 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
+import java.util.HashMap;
 
 @Document(collection = "Activity")
 public class Activity {
@@ -13,12 +14,70 @@ public class Activity {
     private String type;
     private int calories;
     private int minutes;
+    private HashMap<String, Double> METValues = new HashMap<>();
 
     public Activity(String userId, String type, int calories, int minutes) {
         this.userId = userId;
         this.type = type;
         this.calories = calories;
         this.minutes = minutes;
+        initMET();
+    }
+
+    public void initMET() {
+        METValues.put("bicycling", 7.5);
+        METValues.put("bicycling stationary", 7.0);
+        METValues.put("elliptical", 5.0);
+        METValues.put("rope skipping", 11.0);
+        METValues.put("stair treadmill", 9.0);
+        METValues.put("pilates", 3.0);
+        METValues.put("rowing", 6.0);
+        METValues.put("aerobics", 7.3);
+        METValues.put("jogging", 7.0);
+        METValues.put("running 4.0", 6.0);
+        METValues.put("running 5.0", 8.3);
+        METValues.put("running 5.2", 9.0);
+        METValues.put("running 6.0", 9.8);
+        METValues.put("running 6.7", 10.5);
+        METValues.put("running 7.0", 11.0);
+        METValues.put("running 7.5", 11.8);
+        METValues.put("running 8.0", 11.8);
+        METValues.put("running 8.6", 12.3);
+        METValues.put("running 9.0", 12.8);
+        METValues.put("running 10.0", 14.5);
+        METValues.put("running 11.0", 16.0);
+        METValues.put("running 12.0", 19.0);
+        METValues.put("running 13.0", 19.8);
+        METValues.put("running 14.0", 23.0);
+        METValues.put("basketball", 6.5);
+        METValues.put("boxing", 5.5);
+        METValues.put("badminton", 5.5);
+        METValues.put("football", 8.0);
+        METValues.put("golf", 4.8);
+        METValues.put("soccer", 7.0);
+        METValues.put("softball", 5.0);
+        METValues.put("tennis", 7.3);
+        METValues.put("volleyball", 3.0);
+        METValues.put("wrestling", 6.0);
+        METValues.put("canoeing", 3.5);
+        METValues.put("kayaking", 5.0);
+        METValues.put("jet skiing", 7.0);
+        METValues.put("scuba diving", 7.0);
+        METValues.put("surfing", 3.0);
+        METValues.put("swimming", 6.0);
+        METValues.put("ice skating", 7.0);
+        METValues.put("skiing", 7.0);
+        METValues.put("sledding", 7.0);
+        METValues.put("snowboarding", 4.3);
+        METValues.put("ice hockey", 8.0);
+    }
+
+    public HashMap<String, Double> getMETValues() {
+        return METValues;
+    }
+
+    public void setMETValues(HashMap<String, Double> METValues) {
+        this.METValues = METValues;
     }
 
     public String getId() {
@@ -35,6 +94,11 @@ public class Activity {
 
     public int getCalories() {
         return calories;
+    }
+
+
+    public void setCalories(int calories) {
+        this.calories = calories;
     }
 
     public int getMinutes() {
