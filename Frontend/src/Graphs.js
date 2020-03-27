dd6646df42a1b3ef400f241b86223975aa6f65d4
Linnea <linneallindstrom@hotmail.com>
2020-03-26 22:47:28 -0400

    Display exercise calories graph and toggle

--- Frontend/src/Graphs.js
+++ Frontend/src/Graphs.js
@@ -26,6 +26,7 @@ import {
 } from 'victory-native';
 import URL from './url';
 import BUTTONS from './styles/buttons';
+import COLORS from './styles/colors';
 
 let today = new Date();
 let day = today.getDay();
@@ -51,8 +52,12 @@ export default class Graphs extends Component {
     super(props);
     this.state = {
       isLoading: true,
-      text: 'default',
-      graphData: [],
+      timeGraphData: [],
+      caloriesGraphData: [],
+      currentGraph: [],
+      timeButtonStyle: styles.active,
+      caloriesButtonStyle: styles.inactive,
+      timeGraph: true,
     };
   }
   componentDidMount(): void {
@@ -60,14 +65,42 @@ export default class Graphs extends Component {
     const {userId} = route.params;
     fetch(
       Platform.OS === 'android'
-        ? `${URL.heroku}/User/weeksExercise?userId=${userId}`
-        : `http://localhost:8080/User/getCharacteristics?userId=${userId}`,
+        ? `${URL.heroku}/User/weekExerciseTime?userId=${userId}`
+        : `http://localhost:8080/User/weekExerciseTime?userId=${userId}`,
+    )
+      .then(res => res.text())
+      .then(data =>
+        this.setState({
+          timeGraphData: [
+            {day: 1, minutes: Number(data.split(' ')[0])},
+            {day: 2, minutes: Number(data.split(' ')[1])},
+            {day: 3, minutes: Number(data.split(' ')[2])},
+            {day: 4, minutes: Number(data.split(' ')[3])},
+            {day: 5, minutes: Number(data.split(' ')[4])},
+            {day: 6, minutes: Number(data.split(' ')[5])},
+            {day: 7, minutes: Number(data.split(' ')[6])},
+          ],
+          currentGraph: [
+            {day: 1, minutes: Number(data.split(' ')[0])},
+            {day: 2, minutes: Number(data.split(' ')[1])},
+            {day: 3, minutes: Number(data.split(' ')[2])},
+            {day: 4, minutes: Number(data.split(' ')[3])},
+            {day: 5, minutes: Number(data.split(' ')[4])},
+            {day: 6, minutes: Number(data.split(' ')[5])},
+            {day: 7, minutes: Number(data.split(' ')[6])},
+          ],
+        }),
+      );
+    fetch(
+      Platform.OS === 'android'
+        ? `${URL.heroku}/User/weekExerciseCalories?userId=${userId}`
+        : `http://localhost:8080/User/weekExerciseCalories?userId=${userId}`,
     )
       .then(res => res.text())
       .then(data =>
         this.setState({
           isLoading: false,
-          graphData: [
+          caloriesGraphData: [
             {day: 1, minutes: Number(data.split(' ')[0])},
             {day: 2, minutes: Number(data.split(' ')[1])},
             {day: 3, minutes: Number(data.split(' ')[2])},
@@ -80,10 +113,25 @@ export default class Graphs extends Component {
       );
   }
 
-  onPress = () => {
-    this.setState({
-      text: 'changed',
-    });
+  timeGraph = () => {
+    if (!this.state.timeGraph) {
+      this.setState({
+        timeButtonStyle: styles.active,
+        caloriesButtonStyle: styles.inactive,
+        timeGraph: true,
+        currentGraph: this.state.timeGraphData,
+      });
+    }
+  };
+  caloriesGraph = () => {
+    if (this.state.timeGraph) {
+      this.setState({
+        timeButtonStyle: styles.inactive,
+        caloriesButtonStyle: styles.active,
+        timeGraph: false,
+        currentGraph: this.state.caloriesGraphData,
+      });
+    }
   };
 
   render() {
@@ -94,6 +142,12 @@ export default class Graphs extends Component {
         </View>
       );
     }
+    // const timeVictoryBar = (
+    //   <VictoryBar data={this.state.timeGraphData} x="day" y="minutes" />
+    // );
+    // const caloriesVictoryBar = (
+    //   <VictoryBar data={this.state.caloriesGraphData} x="day" y="minutes" />
+    // );
     return (
       <View style={styles.container}>
         <VictoryChart
@@ -115,7 +169,7 @@ export default class Graphs extends Component {
           />
           <VictoryAxis
             dependentAxis
-            label="minutes"
+            label={this.state.timeGraph ? 'minutes' : 'calories'}
             // scale={{y: 'time'}}
             style={{
               axisLabel: {padding: 40},
@@ -123,11 +177,23 @@ export default class Graphs extends Component {
             // tickFormat specifies how ticks should be displayed
             // tickFormat={x => `${x} min`}
           />
-          <VictoryBar data={this.state.graphData} x="day" y="minutes" />
+          <VictoryBar data={this.state.currentGraph} x="day" y="minutes" />
         </VictoryChart>
-        <TouchableOpacity style={BUTTONS.primaryButton} onPress={this.onPress}>
-          <Text style={BUTTONS.primaryButtonText}>{this.state.text}</Text>
-        </TouchableOpacity>
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity
+            activeOpacity={1}
+            style={this.state.timeButtonStyle}
+            disabled={this.state.timeGraph}
+            onPress={this.timeGraph}>
+            <Text style={styles.text}>time</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={1}
+            style={this.state.caloriesButtonStyle}
+            onPress={this.caloriesGraph}>
+            <Text style={styles.text}>calories</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -139,5 +205,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#f5fcff',
+  },
+  active: {
+    backgroundColor: '#455a64',
+    padding: 15,
+    margin: 5,
+    width: 100,
+    borderRadius: 40,
+  },
+  inactive: {
+    backgroundColor: '#b0bec5',
+    padding: 15,
+    margin: 5,
+    width: 100,
+    borderRadius: 40,
+  },
+  text: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    textAlign: 'center',
   },
 });
