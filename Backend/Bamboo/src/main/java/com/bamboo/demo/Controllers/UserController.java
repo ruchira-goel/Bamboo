bb84e5b8e81ca7d50f3c5856872a6945ba39e03a
Linnea <linneallindstrom@hotmail.com>
2020-03-30 22:07:22 -0400

    Merge graph-backend

--- Backend/Bamboo/src/main/java/com/bamboo/demo/Controllers/UserController.java
+++ Backend/Bamboo/src/main/java/com/bamboo/demo/Controllers/UserController.java
@@ -118,3 +118,19 @@ public class UserController {
         return userHandler.changePass(userId, encryptedPassword);
     }
 }
+
+    @RequestMapping("/User/weekExerciseTime")
+    public String weekExerciseTime(@RequestParam(value = "userId") String userId) {
+        return userHandler.getWeekExerciseTime(userId);
+    }
+
+    @RequestMapping("/User/weekExerciseCalories")
+    public String weekExerciseCalories(@RequestParam(value = "userId") String userId) {
+        return userHandler.getWeekExerciseCalories(userId);
+    }
+
+    @RequestMapping("/User/weekCaloriesConsumption")
+    public String weekCaloriesConsumption(@RequestParam(value = "userId") String userId) {
+        return userHandler.getWeekCaloriesConsumption(userId);
+    }
+}
