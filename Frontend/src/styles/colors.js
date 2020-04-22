a9266cfc0188fbd7c43d6a7162039f6b02a6146e
Ruchira <ruchira.goel13@gmail.com>
2020-04-21 20:12:46 -0400

    Only backend files from master

--- Frontend/src/styles/colors.js
+++
@@ -1,7 +0,0 @@
-const COLORS = {
-    primaryColor: '#aaddaa',
-    secondaryColor: '#00c880',
-    // errorColor
-};
-
-export default COLORS;
--- Frontend/src/url.js
+++
@@ -1,6 +0,0 @@
-const URL = {
-  android: 'http://10.0.2.2:8080',
-  ios: 'http://localhost:8080',
-};
-
-export default URL;
