b7042a9a385c35d73a6260fbb5fc1f887e953cb2
Linnea <linneallindstrom@hotmail.com>
2020-04-24 21:47:39 -0400

    Style page

--- Frontend/src/RecommendedMealsList.js
+++ Frontend/src/RecommendedMealsList.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import {Text, View, StyleSheet, FlatList, TouchableOpacity, Platform, Alert, ScrollView} from 'react-native';
-import {useNavigation, useRoute} from "@react-navigation/native";
-import * as Constants from "./Constants";
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+  Alert,
+  ScrollView,
+} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import * as Constants from './Constants';
 
 // Sources:
 // https://reactnative.dev/docs/images
@@ -13,172 +22,122 @@ import * as Constants from "./Constants";
 // https://www.tutorialspoint.com/react_native/react_native_listview.htm
 
 class RecommendedMealsList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userId: '',
-            ingredients: '',
-            instructions: '',
-            meals: [],
-        };
-    }
-
-    render() {
-        const {route} = this.props;
-        const {meals} = route.params;
-        //const {meals} = ['hi', 'hello']
-        return (
-          <ScrollView>
-            <FlatList
-                data={meals}
-                //style={{width: '70%'}}
-                renderItem={({item}) => (
-                    <View>
-                        <TouchableOpacity
-                            style={{
-                                flex: 1,
-                                justifyContent: 'center',
-                                alignItems: 'center',
-                                backgroundColor: 'darkseagreen',
-                            }}
-                        onPress={() => this.getInstructionsAndIngredients(item.id)}>
-                            <Text>{item.name}</Text>
-                            {this.renderNutrient('Calories', item.calories.toFixed(2))}
-                            {this.renderNutrient('Fat', item.fat.toFixed(2))}
-                            {this.renderNutrient('Protein', item.protein.toFixed(2))}
-                            {this.renderNutrient('Carbs', item.carbs.toFixed(2))}
-                        </TouchableOpacity>
-                        <View style={{padding: '2%'}} />
-                    </View>
-                )}
-            />
-          </ScrollView>
-        );
-    }
-
-    getInstructionsAndIngredients = (mealId) => {
-        const {route} = this.props;
-        const {userId} = route.params;
-        fetch(
-            Platform.OS === 'android'
-                ? `${
-                    Constants.URL.android
-                }/Meal/getIng?mealId=${mealId}&userId=${userId}`
-                : `${Constants.URL.ios}/Meal/getIng?mealId=${mealId}&userId=${userId}`,
-        )
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                if (data.error) {
-                    Alert.alert(
-                        data.message,
-                        'Unable to find this meal',
-                        [{text: 'OK'}],
-                    );
-                    return;
-                } else {
-                    this.setState({ingredients: data}, () => {
-                        fetch(
-                            Platform.OS === 'android'
-                                ? `${
-                                    Constants.URL.android
-                                }/Meal/getIns?mealId=${mealId}`
-                                : `${Constants.URL.ios}/Meal/getIns?mealId=${mealId}`,
-                        )
-                            .then(res => res.json())
-                            .then(data => {
-                                console.log(data);
-                                if (data.error) {
-                                    Alert.alert(
-                                        data.message,
-                                        'Unable to find this meal',
-                                        [{text: 'OK'}],
-                                    );
-                                    return;
-                                } else {
-                                    this.setState({instructions: data}, () => {
-                                        this.props.navigation.navigate('Meal Instructions', {
-                                            instructions: data,
-                                            ingredients: this.state.ingredients
-                                        })
-                                    })
-                                }
-                            });
-                    })
-                }
-            });
-
-    }
-
-    renderNutrient = (nutrient, value) => {
-        console.log(value);
-        if (value !== '0.00') {
-            let unit = 'g';
-            if (nutrient === 'Calories') {
-                unit = '';
-            }
-            return (
-                <View style={{flex: 1}}>
-                    <Text>
-                        {nutrient}: {value}
-                        {unit}
-                    </Text>
-                </View>
-            );
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: '',
+      ingredients: '',
+      instructions: '',
+      meals: [],
     };
+  }
+
+  render() {
+    const {route} = this.props;
+    const {meals} = route.params;
+    return (
+      <ScrollView>
+        <FlatList
+          data={meals}
+          renderItem={({item}) => (
+            <View>
+              <TouchableOpacity
+                style={styles.meal}
+                onPress={() => this.getInstructionsAndIngredients(item.id)}>
+                <Text style={styles.text}>{item.name}</Text>
+                {this.renderNutrient('Calories', item.calories.toFixed(2))}
+                {this.renderNutrient('Fat', item.fat.toFixed(2))}
+                {this.renderNutrient('Protein', item.protein.toFixed(2))}
+                {this.renderNutrient('Carbs', item.carbs.toFixed(2))}
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+        <View style={{marginTop: 5}} />
+      </ScrollView>
+    );
+  }
+
+  getInstructionsAndIngredients = mealId => {
+    const {route} = this.props;
+    const {userId} = route.params;
+    fetch(
+      Platform.OS === 'android'
+        ? `${
+            Constants.URL.android
+          }/Meal/getIng?mealId=${mealId}&userId=${userId}`
+        : `${Constants.URL.ios}/Meal/getIng?mealId=${mealId}&userId=${userId}`,
+    )
+      .then(res => res.json())
+      .then(data => {
+        if (data.error) {
+          Alert.alert(data.message, 'Unable to find this meal', [{text: 'OK'}]);
+        } else {
+          this.setState({ingredients: data}, () => {
+            fetch(
+              Platform.OS === 'android'
+                ? `${Constants.URL.android}/Meal/getIns?mealId=${mealId}`
+                : `${Constants.URL.ios}/Meal/getIns?mealId=${mealId}`,
+            )
+              .then(res => res.json())
+              .then(data => {
+                if (data.error) {
+                  Alert.alert(data.message, 'Unable to find this meal', [
+                    {text: 'OK'},
+                  ]);
+                } else {
+                  this.setState({instructions: data}, () => {
+                    this.props.navigation.navigate('Meal Instructions', {
+                      instructions: data,
+                      ingredients: this.state.ingredients,
+                    });
+                  });
+                }
+              });
+          });
+        }
+      });
+  };
+
+  renderNutrient = (nutrient, value) => {
+    if (value !== '0.00') {
+      let unit = 'g';
+      if (nutrient === 'Calories') {
+        unit = '';
+      }
+      return (
+        <View>
+          <Text>
+            {nutrient}: {value}
+            {unit}
+          </Text>
+        </View>
+      );
+    }
+  };
 }
 
 export default function(props) {
-    const navigation = useNavigation();
-    const route = useRoute();
-    return (
-        <RecommendedMealsList {...props} navigation={navigation} route={route} />
-    );
+  const navigation = useNavigation();
+  const route = useRoute();
+  return (
+    <RecommendedMealsList {...props} navigation={navigation} route={route} />
+  );
 }
 
 const styles = StyleSheet.create({
-    heading: {
-        fontSize: 24,
-        fontWeight: '500',
-        flex: 1,
-        marginTop: '10%',
-    },
-    rowcontainer: {
-        flex: 1,
-        padding: 10,
-        height: 100,
-        marginTop: 3,
-        backgroundColor: '#3eb245',
-        justifyContent: 'center',
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    rowview: {
-        // position: 'absolute',
-        // right: 0,
-        flexDirection: 'row',
-    },
-    textheader: {
-        color: 'black',
-        margin: '10%',
-        textAlign: 'center',
-        fontSize: 24,
-    },
-    text: {
-        margin: 7,
-        color: 'black',
-        fontSize: 18,
-        textAlign: 'center',
-        justifyContent: 'center',
-        textAlignVertical: 'center',
-    },
-    ImageIconStyle: {
-        marginTop: 10,
-        marginLeft: 10,
-        marginRight: 5,
-        marginBottom: 10,
-        height: 25,
-        width: 25,
-    },
+  meal: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: Constants.COLORS.accent.main,
+    marginTop: 5,
+    marginHorizontal: 5,
+    padding: 10,
+  },
+  text: {
+    fontSize: 16,
+    textAlign: 'center',
+  },
 });
