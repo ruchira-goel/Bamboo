78c3c60cd9addb07dcdf2a3f746a4e6ba1dcb53f
Linnea <linneallindstrom@hotmail.com>
2020-04-24 22:02:43 -0400

    Style page

--- Frontend/src/MealInstructions.js
+++ Frontend/src/MealInstructions.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import {Text, View, StyleSheet, FlatList, TouchableOpacity, Platform, Alert, ScrollView} from 'react-native';
-import {useNavigation, useRoute} from "@react-navigation/native";
-import * as Constants from "./Constants";
-import {SafeAreaView} from "react-native-safe-area-context";
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+  Alert,
+  ScrollView,
+} from 'react-native';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import * as Constants from './Constants';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 // Sources:
 // https://reactnative.dev/docs/images
@@ -14,127 +23,75 @@ import {SafeAreaView} from "react-native-safe-area-context";
 // https://www.tutorialspoint.com/react_native/react_native_listview.htm
 
 class MealInstructions extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userId: '',
-            ingredients: '',
-            instructions: '',
-            meals: [],
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: '',
+      ingredients: '',
+      instructions: '',
+      meals: [],
+    };
+  }
 
-    render() {
-        const {route} = this.props;
-        const {ingredients} = route.params;
-        const {instructions} = route.params;
-        console.log(ingredients);
-        return (
-            <View style = {{flex: 1}}>
-                <SafeAreaView>
-                    <ScrollView>
-                        <Text style={{fontSize: 16}}>Ingredients:{'\n'}</Text>
-                        <FlatList
-                            data={ingredients}
-                             scrollEnabled={false}
-                           // style={{backgroundColor: 'yellow'}}
-                            renderItem={({item}) => (
-                            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                                <View
-                                    style={{
-                                        flex: 1,
-                                        //justifyContent: 'center',
-                                        alignItems: 'center',
-                                        alignContent: 'center',
-                                        backgroundColor: 'darkseagreen',
-                                        width: '60%'
-                                    }}>
-                                    <Text style={{textAlign: 'center', fontSize: 16}}>{item}</Text>
-                                </View>
-                                <View style={{padding: '2%'}} />
-                            </View>
-                            )}
-                        />
-                        <Text style={{alignContent: 'center', fontSize: 16}}>{'\n'}Instructions:{'\n'}</Text>
+  render() {
+    const {route} = this.props;
+    const {ingredients} = route.params;
+    const {instructions} = route.params;
+    return (
+      <View style={styles.container}>
+        <SafeAreaView>
+          <ScrollView>
+            <Text style={styles.heading}>Ingredients:{'\n'}</Text>
+            <FlatList
+              data={ingredients}
+              scrollEnabled={false}
+              renderItem={({item}) => (
+                <View style={styles.ingredient}>
+                  <Text style={styles.text}>
+                    {'\u25CF'} {item}
+                  </Text>
+                </View>
+              )}
+            />
+            <Text style={styles.heading}>
+              {'\n'}Instructions:{'\n'}
+            </Text>
 
-                    <FlatList
-                    data={instructions}
-                    //style={{width: '70%'}}
-                    scrollEnabled={false}
-                    renderItem={({item}) => (
-                        <View>
-                            <View
-                                style={{
-                                    flex: 1,
-                                    //justifyContent: 'center',
-                                    //alignItems: 'center',
-                                    //backgroundColor: 'darkseagreen',
-                                }}>
-                                <Text style={{fontSize: 16}}>{item}</Text>
-                            </View>
-                            <View style={{padding: '2%'}} />
-                        </View>
-                    )}
-                />
-                </ScrollView>
-                </SafeAreaView>
-                <View style={{padding: 10}}/>
-            </View>
-
-        );
-    }
+            <FlatList
+              data={instructions}
+              scrollEnabled={false}
+              renderItem={({item}) => (
+                <View style={{marginBottom: 12}}>
+                  <Text style={styles.text}>{item}</Text>
+                </View>
+              )}
+            />
+          </ScrollView>
+        </SafeAreaView>
+      </View>
+    );
+  }
 }
 
 export default function(props) {
-    const navigation = useNavigation();
-    const route = useRoute();
-    return (
-        <MealInstructions {...props} navigation={navigation} route={route} />
-    );
+  const navigation = useNavigation();
+  const route = useRoute();
+  return <MealInstructions {...props} navigation={navigation} route={route} />;
 }
 
 const styles = StyleSheet.create({
-    heading: {
-        fontSize: 24,
-        fontWeight: '500',
-        flex: 1,
-        marginTop: '10%',
-    },
-    rowcontainer: {
-        flex: 1,
-        padding: 10,
-        height: 100,
-        marginTop: 3,
-        backgroundColor: '#3eb245',
-        justifyContent: 'center',
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    rowview: {
-        // position: 'absolute',
-        // right: 0,
-        flexDirection: 'row',
-    },
-    textheader: {
-        color: 'black',
-        margin: '10%',
-        textAlign: 'center',
-        fontSize: 24,
-    },
-    text: {
-        margin: 7,
-        color: 'black',
-        fontSize: 18,
-        textAlign: 'center',
-        justifyContent: 'center',
-        textAlignVertical: 'center',
-    },
-    ImageIconStyle: {
-        marginTop: 10,
-        marginLeft: 10,
-        marginRight: 5,
-        marginBottom: 10,
-        height: 25,
-        width: 25,
-    },
+  container: {
+    flex: 1,
+    paddingHorizontal: 10,
+  },
+  heading: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  ingredient: {
+    marginBottom: 4,
+  },
+  text: {
+    fontSize: 16,
+  },
 });
